292a5faa20fc2c9bfd44ecb33e941925b45b1b77
mahmouudKhalled <foktop018@gmail.com>
2024-11-13 23:59:57 +0200

    files modify

--- src/Components/MainSlider.jsx
+++ src/Components/MainSlider.jsx
@@ -1,5 +1,5 @@
 import Slider from "react-slick";
-import ProductItem from "./productItem";
+import ProductItem from "./ProductItem";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ProductsContext } from "./../Contexts/ProuductsContext";
